c9f6c96c6be5b484482e37914807f14881b57abb
Esther <esther2180@gmail.com>
2018-10-11 17:22:43 -0700

    Init Firebase Set-Up

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
 import types from './types';
+import { db } from '../firebase';
+
+export const getMessages = () => dispatch => {
+    db.ref('/').on('value', (snapshot) => {
+        console.log('DB Snapshot:', snapshot.val());
+    });
+}
